8dc187a0b1f5459931c8b463fd16a130cfad8a20
JungJeaHoon <128768425+JungJeaHoon@users.noreply.github.com>
2023-03-30 18:20:24 +0900

    Hanwha_sub1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const header =document.querySelector('header')
+// window.addEventListener('scroll',function(){
+    
+//     if(this.window.scrollY > 800){  
+//         header.classList.add('on');
+//     }else{
+//         header.classList.remove('on')
+//     }
+// })
+
+alert('팀회원 로그인 후 신청 가능합니다.');
